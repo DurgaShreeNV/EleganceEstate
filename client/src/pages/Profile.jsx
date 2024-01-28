5009b568c2d23677bb3fa80235eced6546b0edae
DurgaShreeNV <durgashreenv569@gmail.com>
2024-01-28 19:17:59 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,27 +1,91 @@
-//This import is required in order to have the user details in the form 
 import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { app } from '../firebase';
 
-export default function Profile()  { 
-  //Lets initialize the current user 
+export default function Profile() {
+  const [file, setFile] = useState(undefined);
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user)
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  console.log(formData);
+  console.log(filePerc);
+  console.log(fileUploadError);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+
+      (error) => {
+        setFileUploadError(true);
+      },
+
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+          ((downloadURL) =>
+            setFormData({ ...formData, avatar: downloadURL })
+          );
+      }
+    );
+  };
+
   return (
-    //To have the entire profile data with a little padding, and alligned to the center(mx-auto):
     <div className='p-3 max-w-lg mx-auto'>
-      {/* Lets have heading with a font size of 3xl, lets make it semi bold and have it centered.  */}
-      <h1 className = 'text-3xl font-semibold text-center my-7'>Profile</h1>
-      {/* Lets have a form without any action as we are just displaying the profile picture, name, etc. Import the useSelector */}
-      {/* Also lets add the required amount of gap between the username, email, etc.  */}
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        {/* for the image, lets have the image rounded, whenever we move the cursor on the image there should be some effect, similar to having the text at the center we can have the image at the image at the center using self center*/}
-        <img src = {currentUser.avatar} alt = "profile" 
-        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
-        {/* Lets add the placeholder and id, basically representing what ever they are storing  */}
-        <input type="text" placeholder='username' id = 'username' className='border p-3 rounded-lg' />
-        <input type="email" placeholder='email' id = 'email' className='border p-3 rounded-lg' />
-        <input type="text" placeholder='password' id = 'password' className='border p-3 rounded-lg' />
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden accept='image/*'
+        />
+
+        <img
+          onClick={() => fileRef.current.click()}
+          //Once profile picture is updated, we have to see the new profile picture 
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        {/* Required conditions in this p */}
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>Error Image upload (Image must be less than 2 mb)</span>) :
+            filePerc > 0 && filePerc < 100 ? (
+              <span className='text-slate-700'>
+                {'Uploading ${filePerc}&'}
+              </span>) :
+              filePerc === 100 ? (
+                <span className='text-green-700'>Image successfully uploaded!</span>)
+                :
+                (
+                  ''
+                )
+          }
+        </p>
+
+
+        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
+        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
+        <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg' />
         <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
       </form>
-      {/* Lets add delete account and sign out  */}
       <div className="flex justify-between mt-5">
         <span className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
